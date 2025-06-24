42967c9bc38644aa635bbc5e25ffbacf8567daaf
gaetan.laboure <gaetan.laboure@soprasteria.com>
2025-06-24 17:56:42 +0200

    init api

--- api/src/terraform/main.tf
+++ api/src/terraform/main.tf
@@ -78,9 +78,7 @@ terraform {
 
 provider "azurerm" {
   features {}
-  resource_provider_registrations = {
-    "Microsoft.Storage" "Microsoft.ApiManagement"
-  }
+  resource_provider_registrations = ["Microsoft.Storage", "Microsoft.ApiManagement"]
   subscription_id = var.AZ_SUB_ID
   client_id       = var.AZ_CLIENT_ID
   client_secret   = var.AZ_CLIENT_SECRET

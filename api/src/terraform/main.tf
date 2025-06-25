4219a118db4e325581c962362fd414609b2033bb
gaetan.laboure <gaetan.laboure@soprasteria.com>
2025-06-25 13:53:49 +0200

    Update main.tf

--- api/src/terraform/main.tf
+++ api/src/terraform/main.tf
@@ -1,29 +1,67 @@
-# -----------------------------------------------------
-# Variables - Projet
-# -----------------------------------------------------
+# ######
+# ## Variables set in the project level
+# ######
 
-variable "APIM_RG"                { type = string }
-variable "APIM_NAME"              { type = string }
-variable "AWS_DEFAULT_REGION"     { type = string }
-variable "AZ_TENANT_ID"           { type = string }
-variable "AZ_SUB_ID"              { type = string }
-variable "AZ_CLIENT_ID"           { type = string }
-variable "AZ_CLIENT_SECRET"       { type = string }
+variable "APIM_RG" {
+  type = string
+}
 
-# -----------------------------------------------------
-# Variables - CICD
-# -----------------------------------------------------
+variable "APIM_NAME" {
+  type = string
+}
 
-variable "APIM_PRODUCT"           { type = string }
-variable "API_GATEWAY_ENDPOINT"   { type = string }
-variable "API_SYSTEM_NAME"        { type = string }
-variable "API_VERSION"            { type = string }
-variable "ENV"                    { type = string }
-variable "SLS_NAME"               { type = string }
 
-# -----------------------------------------------------
-# Terraform Providers et Backend
-# -----------------------------------------------------
+variable "AWS_DEFAULT_REGION" {
+  type = string
+}
+
+variable "AZ_TENANT_ID" {
+  type = string
+}
+
+variable "AZ_SUB_ID" {
+  type = string
+}
+
+variable "AZ_CLIENT_ID" {
+  type = string
+}
+
+variable "AZ_CLIENT_SECRET" {
+  type = string
+}
+
+# ######
+# ## Variables set during the CICD
+# ######
+
+variable "APIM_PRODUCT" {
+  type = string
+}
+
+variable "API_GATEWAY_ENDPOINT" {
+  type = string
+}
+
+variable "API_SYSTEM_NAME" {
+  type = string
+}
+
+variable "API_VERSION" {
+  type = string
+}
+
+variable "ENV" {
+  type = string
+}
+
+variable "SLS_NAME" {
+  type = string
+}
+
+# ######
+# ## Providers declaration
+# ######
 
 terraform {
   required_providers {
@@ -31,21 +69,26 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">= 3.80.0"
     }
+  }
+}
+
+terraform {
+  required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.0.0"
+      version = "=5.70.0"
     }
-  }
-
-  backend "s3" {
-    bucket = "nom-de-ton-bucket"
-    key    = "project/${var.APIM_PRODUCT}/${var.ENV}/ms/${var.SLS_NAME}.tfstate"
-    region = "eu-west-3"
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.73.0"
+    }
   }
 }
 
 provider "azurerm" {
+  skip_provider_registration = true
   features {}
+
   subscription_id = var.AZ_SUB_ID
   client_id       = var.AZ_CLIENT_ID
   client_secret   = var.AZ_CLIENT_SECRET
@@ -53,42 +96,47 @@ provider "azurerm" {
 }
 
 provider "aws" {
-  region = var.AWS_DEFAULT_REGION
+  region     = var.AWS_DEFAULT_REGION
 }
 
-# -----------------------------------------------------
-# External - Récupération du titre depuis asyncapi.json
-# -----------------------------------------------------
+terraform {
+  backend "s3" {
+    bucket = "%bucket%"
+    key    = "project/%TF_VAR_APIM_PRODUCT%/%ENV%/ms/%SLS_NAME%.tfstate"
+    region = "%AWS_DEFAULT_REGION%"
+  }
+}
+
+# ######
+# ## External data source to extract title from asyncapi.json
+# ######
 
 data "external" "asyncapi_title" {
-  program = [
-    "sh", "-c",
-    "jq -n --rawfile title ../api/asyncapi.json '{\"title\": ( ($title | fromjson).info.title )}'"
-  ]
+  program = ["sh", "-c", "jq -n --rawfile title ../api/asyncapi.json '{\"title\": ($title | fromjson | .info.title)}'"]
 }
 
-# -----------------------------------------------------
-# Azure API Management API
-# -----------------------------------------------------
+# ######
+# ## Azure resources
+# ######
 
 resource "azurerm_api_management_api" "ics_api" {
   name                = var.SLS_NAME
   resource_group_name = var.APIM_RG
   api_management_name = var.APIM_NAME
   revision            = "1"
-
-  display_name = "${data.external.asyncapi_title.result.title} - ${var.API_VERSION}"
-  path         = "api/${var.API_VERSION}/${var.API_SYSTEM_NAME}"
-  protocols    = ["wss"]
-  api_type     = "websocket"
-  service_url  = var.API_GATEWAY_ENDPOINT
+  display_name        = "${data.external.asyncapi_title.result.title} - ${var.API_VERSION}"
+  path                = "api/${var.API_VERSION}/${var.API_SYSTEM_NAME}"
+  protocols           = ["wss"]
+  service_url         = var.API_GATEWAY_ENDPOINT
+  api_type            = "websocket" # Specify the API type as WebSocket
 
   subscription_key_parameter_names {
     header = "api-key"
     query  = "api-key"
   }
-
+ 
   description = "Documentation : https://${var.APIM_NAME}.blob.core.windows.net/${var.API_SYSTEM_NAME}/index.html"
+
 }
 
 resource "azurerm_api_management_product_api" "ics_product_api" {
@@ -98,56 +146,46 @@ resource "azurerm_api_management_product_api" "ics_product_api" {
   resource_group_name = var.APIM_RG
 }
 
-# -----------------------------------------------------
-# Azure Storage pour documentation API
-# -----------------------------------------------------
-
 resource "azurerm_storage_account" "ics_products_documentation" {
-  name                     = lower(replace(var.APIM_NAME, "-", ""))
+  name                     = var.APIM_NAME
   resource_group_name      = var.APIM_RG
   location                 = "francecentral"
-  account_tier             = "Standard"
+  account_tier            = "Standard"
   account_replication_type = "LRS"
 }
 
 resource "azurerm_storage_container" "docs" {
   name                  = var.API_SYSTEM_NAME
-  storage_account_id    = azurerm_storage_account.ics_products_documentation.id
+  storage_account_name  = azurerm_storage_account.ics_products_documentation.name
   container_access_type = "blob"
 }
 
-# Upload des fichiers HTML
 resource "azurerm_storage_blob" "api_docs" {
-  for_each = { for file in fileset("output", "*") : file => file }
-
-  name                   = each.key
-  storage_account_name   = azurerm_storage_account.ics_products_documentation.name
+  count                 = length(tolist(fileset("output", "*")))
+  name                  = basename(tolist(fileset("output", "*"))[count.index])
+  storage_account_name  = azurerm_storage_account.ics_products_documentation.name
   storage_container_name = azurerm_storage_container.docs.name
-  type                   = "Block"
-  source                 = "output/${each.key}"
-  content_type           = "text/html"
+  type                  = "Block"
+  source                = "output/${basename(tolist(fileset("output", "*"))[count.index])}"
+  content_type          = "text/html"
 }
 
-# Upload des fichiers CSS
 resource "azurerm_storage_blob" "api_docs_css" {
-  for_each = { for file in fileset("output/css", "*") : file => file }
-
-  name                   = "css/${each.key}"
-  storage_account_name   = azurerm_storage_account.ics_products_documentation.name
+  count                 = length(tolist(fileset("output/css", "*")))
+  name                  = "css/${basename(tolist(fileset("output/css", "*"))[count.index])}"
+  storage_account_name  = azurerm_storage_account.ics_products_documentation.name
   storage_container_name = azurerm_storage_container.docs.name
-  type                   = "Block"
-  source                 = "output/css/${each.key}"
-  content_type           = "text/css"
+  type                  = "Block"
+  source                = "output/css/${basename(tolist(fileset("output/css", "*"))[count.index])}"
+  content_type          = "text/css"
 }
 
-# Upload des fichiers JS
 resource "azurerm_storage_blob" "api_docs_js" {
-  for_each = { for file in fileset("output/js", "*") : file => file }
-
-  name                   = "js/${each.key}"
-  storage_account_name   = azurerm_storage_account.ics_products_documentation.name
+  count                 = length(tolist(fileset("output/js", "*")))
+  name                  = "js/${basename(tolist(fileset("output/js", "*"))[count.index])}"
+  storage_account_name  = azurerm_storage_account.ics_products_documentation.name
   storage_container_name = azurerm_storage_container.docs.name
-  type                   = "Block"
-  source                 = "output/js/${each.key}"
-  content_type           = "application/javascript"
+  type                  = "Block"
+  source                = "output/js/${basename(tolist(fileset("output/js", "*"))[count.index])}"
+  content_type          = "application/javascript"
 }
